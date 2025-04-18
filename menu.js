fd956b940402f72a8abfbe69e720fc1117a35ab0
SilverTosh <silvestermigwi76@gmail.com>
2025-04-18 21:09:56 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,36 @@
+// Commands for KINGVON XMD
+module.exports = async function menu() {
+  return `╭═══『 KI KINGVON-XMD 』══⊷
+║ MAIN MENU
+║ - .menu
+║ - .ping
+║ - .sticker
+║ - .vv
+║ - .antidelete on/off
+║ - .support
+║ ━━━ GROUP COMMANDS ━━━
+║ - .tag
+║ - .hidetag
+║ - .remove [country_code]
+║ - .del
+║ - .opentime [time]
+║ - .closetime [time]
+║ - .promote @user
+║ - .demote @user
+║ - .gclink
+║ - .antilink on/off
+║ - .terminate
+║ ━━━ VON SPECIAL ━━━
+║ - .danger
+║ - .grandson
+║ - .addnew
+║ ━━━ OWNER SETTINGS ━━━
+║ - .update
+║ - .restart
+║ ━━━ USER SETTINGS ━━━
+║ - .autolike status
+║ - .autoread status
+║ - .autolike message
+║ - .autoread message
+╰═════════════════⊷`;
+};
